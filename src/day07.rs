340af19dc0a7c4797f88ea3095f1e38d3bc6cc64
Alan Briolat <alan.briolat@gmail.com>
2019-12-07 15:19:43 +0000

    Day 7 cleanups and renaming

--- src/day07.rs
+++ src/day07.rs
@@ -1,26 +1,25 @@
-use std::cmp::max;
 use permutohedron::Heap;
 use crate::intcode::{Word, Emulator};
 
-fn run_amps(base: &Emulator) -> Word {
-    let mut phases: Vec<Word> = (0 .. 5).collect();
-    let heap = Heap::new(&mut phases);
-    let mut best: Word = 0;
-    for permutation in heap {
-        let mut signal: Word = 0;
-        for phase in permutation {
-            let mut amp = base.clone();
-            amp.write(phase);
-            amp.write(signal);
-            amp.run();
-            signal = *amp.read_all().last().unwrap();
-        }
-        best = max(best, signal);
+fn amp_chain(base: &Emulator, phases: &[Word]) -> Word {
+    let mut signal: Word = 0;
+    for phase in phases {
+        let mut amp = base.clone();
+        amp.write(*phase);
+        amp.write(signal);
+        amp.run();
+        signal = *amp.read_all().last().unwrap();
     }
-    return best;
+    return signal;
 }
 
-fn feedback_amps(base: &Emulator, phases: &[Word]) -> Word {
+fn run_amp_chains(base: &Emulator) -> Word {
+    let mut phases: Vec<Word> = (0 .. 5).collect();
+    let heap = Heap::new(&mut phases);
+    heap.map(|phases| amp_chain(base, phases.as_slice())).max().unwrap()
+}
+
+fn amp_feedback_loop(base: &Emulator, phases: &[Word]) -> Word {
     let mut amps: Vec<Emulator> =
         phases
         .iter()
@@ -48,18 +47,18 @@ fn feedback_amps(base: &Emulator, phases: &[Word]) -> Word {
     thruster_signal
 }
 
-fn run_feedback_amps(base: &Emulator) -> Word {
+fn run_amp_feedback_loops(base: &Emulator) -> Word {
     let mut phases: Vec<Word> = (5 .. 10).collect();
     let heap = Heap::new(&mut phases);
-    heap.map(|phases| feedback_amps(base, phases.as_slice())).max().unwrap()
+    heap.map(|phases| amp_feedback_loop(base, phases.as_slice())).max().unwrap()
 }
 
 pub fn part1() -> i32 {
-    run_amps(&Emulator::from_data_file("day07_input.txt"))
+    run_amp_chains(&Emulator::from_data_file("day07_input.txt"))
 }
 
 pub fn part2() -> i32 {
-    run_feedback_amps(&Emulator::from_data_file("day07_input.txt"))
+    run_amp_feedback_loops(&Emulator::from_data_file("day07_input.txt"))
 }
 
 #[cfg(test)]
@@ -67,38 +66,35 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_run_amps_1() {
+    fn test_run_amp_chains_1() {
         let base = Emulator::from_data_file("day07_example1.txt");
-        let result = run_amps(&base);
-        assert_eq!(result, 43210);
+        assert_eq!(run_amp_chains(&base), 43210);
     }
 
     #[test]
-    fn test_run_amps_2() {
+    fn test_run_amp_chains_2() {
         let base = Emulator::from_data_file("day07_example2.txt");
-        let result = run_amps(&base);
-        assert_eq!(result, 54321);
+        assert_eq!(run_amp_chains(&base), 54321);
     }
 
     #[test]
-    fn test_run_amps_3() {
+    fn test_run_amp_chains_3() {
         let base = Emulator::from_data_file("day07_example3.txt");
-        let result = run_amps(&base);
-        assert_eq!(result, 65210);
+        assert_eq!(run_amp_chains(&base), 65210);
     }
 
     #[test]
-    fn test_run_feedback_amps_1() {
+    fn test_run_amp_feedback_loops_1() {
         let base = Emulator::from_data_file("day07_example4.txt");
-        assert_eq!(feedback_amps(&base, &[9, 8, 7, 6, 5]), 139629729);
-        assert_eq!(run_feedback_amps(&base), 139629729);
+        assert_eq!(amp_feedback_loop(&base, &[9, 8, 7, 6, 5]), 139629729);
+        assert_eq!(run_amp_feedback_loops(&base), 139629729);
     }
 
     #[test]
-    fn test_run_feedback_amps_2() {
+    fn test_run_amp_feedback_loops_2() {
         let base = Emulator::from_data_file("day07_example5.txt");
-        assert_eq!(feedback_amps(&base, &[9, 7, 8, 5, 6]), 18216);
-        assert_eq!(run_feedback_amps(&base), 18216);
+        assert_eq!(amp_feedback_loop(&base, &[9, 7, 8, 5, 6]), 18216);
+        assert_eq!(run_amp_feedback_loops(&base), 18216);
     }
 
     #[test]
