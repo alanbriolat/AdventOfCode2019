51da618e9666c32ed6329e5d18d5803a6202be84
Alan Briolat <alan.briolat@gmail.com>
2019-12-08 11:12:12 +0000

    Day 8 part 2

--- src/day08.rs
+++ src/day08.rs
@@ -3,6 +3,12 @@ use crate::util;
 
 const WIDTH: usize = 25;
 const HEIGHT: usize = 6;
+const SIZE: usize = WIDTH * HEIGHT;
+
+#[allow(dead_code)]
+const BLACK: u8 = b'0';
+const WHITE: u8 = b'1';
+const TRANSPARENT: u8 = b'2';
 
 fn count_bytes(layer: &[u8]) -> HashMap<u8, usize> {
     let mut out = HashMap::new();
@@ -13,15 +19,34 @@ fn count_bytes(layer: &[u8]) -> HashMap<u8, usize> {
     return out;
 }
 
+fn merge_layers(current: &mut [u8], next: &[u8]) {
+    for (a, b) in current.iter_mut().zip(next.iter()) {
+        if *a == TRANSPARENT {
+            *a = *b;
+        }
+    }
+}
+
 pub fn part1() -> usize {
     let data = util::read_lines("day08_input.txt").into_iter().nth(0).unwrap();
-    let counters: Vec<HashMap<u8, usize>> = data.as_bytes().chunks(WIDTH * HEIGHT).map(|chunk| count_bytes(chunk)).collect();
+    let counters: Vec<HashMap<u8, usize>> = data.as_bytes().chunks(SIZE).map(|chunk| count_bytes(chunk)).collect();
     let most_zeroes = counters.iter().min_by_key(|x| x.get(&b'0').or(Some(&0)).unwrap()).unwrap();
     most_zeroes.get(&b'1').unwrap() * most_zeroes.get(&b'2').unwrap()
 }
 
-pub fn part2() -> i32 {
-    0
+pub fn part2() -> String {
+    let data = util::read_lines("day08_input.txt").into_iter().nth(0).unwrap();
+    let layers: Vec<&[u8]> = data.as_bytes().chunks(SIZE).collect();
+    let mut current: [u8; SIZE] = [TRANSPARENT; SIZE];
+    for layer in layers {
+        merge_layers(&mut current, layer);
+    }
+    let strings: Vec<String> =
+        current
+        .chunks(WIDTH)
+        .map(|x| x.iter().map(|c| if *c == WHITE { 'X' } else { ' ' }).collect())
+        .collect();
+    format!("\n{}\n", strings.join("\n"))
 }
 
 #[cfg(test)]
@@ -35,6 +60,13 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(), unimplemented!());
+        assert_eq!(part2(), format!("\n{}\n", vec![
+            "XXXX X  X   XX X  X X    ",
+            "X    X  X    X X  X X    ",
+            "XXX  XXXX    X X  X X    ",
+            "X    X  X    X X  X X    ",
+            "X    X  X X  X X  X X    ",
+            "X    X  X  XX   XX  XXXX ",
+        ].join("\n")));
     }
 }
