291934312a6d3e28b25d0fcca79f0d452bc89de9
Alan Briolat <alan.briolat@gmail.com>
2019-12-16 10:27:14 +0000

    Day 16 part 1 (slow)

--- src/day16.rs
+++ src/day16.rs
@@ -1,19 +1,41 @@
 use std::iter::repeat;
+use crate::util;
 
-const BASE_PATTERN: [i8; 4] = [0, 1, 0, -1];
+const BASE_PATTERN: [i32; 4] = [0, 1, 0, -1];
 
-fn generate_pattern(n: usize) -> impl Iterator<Item=i8> {
+fn generate_pattern(n: usize) -> impl Iterator<Item=i32> {
     BASE_PATTERN.iter()             // Iterate over the base pattern
         .cycle()                    // ... repeatedly, forever
         .cloned()                   // Copies, not references
-        .flat_map(move |x| {     // For each element in the base pattern
-            repeat(x).take(n)   // ... repeat it n times
+        .flat_map(move |x| {        // For each element in the base pattern
+            repeat(x).take(n)       // ... repeat it n times
         })
-        .skip(1)                 // Shift the pattern left by one place
+        .skip(1)                    // Shift the pattern left by one place
 }
 
-pub fn part1() -> i32 {
-    0
+fn read_input(filename: &str) -> Vec<i32> {
+    util::read_lines(filename)[0].chars().map(|x| x.to_string().parse().unwrap()).collect()
+}
+
+fn step(input: &[i32]) -> Vec<i32> {
+    (1 ..= input.len())
+        .map(|i| -> i32 {
+            input.iter().cloned()
+                .zip(generate_pattern(i))
+                .map(|(a, b)| { a * b })
+                .sum::<i32>()
+                .abs() % 10
+        })
+        .collect()
+}
+
+pub fn part1() -> String {
+    let mut data = read_input("day16_input.txt");
+    for _ in 0 .. 100 {
+        data = step(data.as_slice());
+    }
+    let output: Vec<String> = data[.. 8].iter().map(|x| format!("{}", x)).collect();
+    output.join("")
 }
 
 pub fn part2() -> i32 {
@@ -30,8 +52,14 @@ mod tests {
     }
 
     #[test]
+    fn test_step() {
+        assert_eq!(step(&[1, 2, 3, 4, 5, 6, 7, 8]), vec![4, 8, 2, 2, 6, 1, 5, 8]);
+        assert_eq!(step(&[4, 8, 2, 2, 6, 1, 5, 8]), vec![3, 4, 0, 4, 0, 4, 3, 8]);
+    }
+
+    #[test]
     fn test_part1() {
-        assert_eq!(part1(), unimplemented!());
+        assert_eq!(part1(), "82525123");
     }
 
     #[test]
